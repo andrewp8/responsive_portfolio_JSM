76fb6368f9e97fff26d7a40f0d84229eeaf178fd
Andrew Pham <andrew.pham.789@gmail.com>
2022-04-30 13:11:45 -0500

    HP - Fixed navbar @media

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -11,8 +11,8 @@ const About = () => {
 	useEffect(() => {
 		const query = '*[_type == "abouts"]';
 
-		client.fetch(query).then((data) =>  
-      setAbouts(data))
+		client.fetch(query)
+		.then((data) => setAbouts(data))
 	}, []);
 
 	return (
