80bfbc8cd3311aa77b70c09be90f7f59abbd3213
Andrew Pham <andrew.pham.789@gmail.com>
2022-04-30 23:41:02 -0500

    HP - 2:42:49 - Work section

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -12,11 +12,24 @@ const Work = () => {
 	const [works, setWorks] = useState([]);
 	const [filterWork, setFilterWork] = useState([]);
 
-	const handleWorkFilter = (item) => {};
+	const handleWorkFilter = (item) => {
+		setActiveFilter(item);
+		setAnimateCard([{ y:100, opacity:0 }]);
+
+		setTimeout(() => {
+			setAnimateCard([{ y:0, opacity:1 }]);
+
+			if (item === 'All') {
+				setFilterWork(works);
+			} else {
+				setFilterWork(works.filter((work) => work.tags.includes(item)));
+			}
+		}, 500);
+	};
 
 	//this useEffect to make React works with Sanity
 	useEffect(() => {
-		const query = '*[_type == "work"]';
+		const query = '*[_type == "works"]';
 
 		client.fetch(query).then((data) => {
 			setWorks(data);
@@ -55,6 +68,45 @@ const Work = () => {
 					<div className="app__work-item app__flex" key={index}>
 						<div className="app__work-img app__flex">
 							<img src={urlFor(work.imgUrl)} alt={work.name} />
+
+							<motion.div
+								whileHover={{ opacity: [0, 1] }}
+								transition={{
+									duration: 0.25,
+									ease: "easeInOut",
+									staggerChildren: 0.5,
+								}}
+								className="app__work-hover app__flex"
+							>
+								<a href={work.projectLink} target="_blank">
+									<motion.div
+										whileInView={{ scale: [0, 1] }}
+										whileHover={{ scale: [1, 0.9] }}
+										transition={{ duration: 0.25 }}
+										className="app__flex"
+									>
+										<AiFillEye />
+									</motion.div>
+								</a>
+								<a href={work.codeLink} target="_blank">
+									<motion.div
+										whileInView={{ scale: [0, 1] }}
+										whileHover={{ scale: [1, 0.9] }}
+										transition={{ duration: 0.25 }}
+										className="app__flex"
+									>
+										<AiFillGithub />
+									</motion.div>
+								</a>
+							</motion.div>
+						</div>
+						<div className="app__work-content app__flex">
+							<h4 className="bold-text">{work.title}</h4>
+							<p className="p-text" style={{ marginTop: 10 }}>{work.description}</p>
+
+							<div className="app__work-tag app__flex">
+								<p className="p-text">{work.tags[0]}</p>
+							</div>
 						</div>
 					</div>
 				))}
@@ -63,4 +115,4 @@ const Work = () => {
 	);
 };
 
-export default Work;
+export default AppWrap(Work, 'work');
